e6b5686dbd85c65b2c162e8e09147549c8244af5
Pavel Smirnov <devil.9207@mail.ru>
2021-11-09 23:08:29 +0300

    first shot

---
+++ homework1/task02.py
@@ -0,0 +1,36 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+    please write function "check_fib", which accepts a Sequence of integers, and
+    returns if the given sequence is a Fibonacci sequence
+
+We guarantee, that the given sequence contain >= 0 integers inside.
+
+"""
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    return True
+
+
+def _check_window(x: int, y: int, z: int) -> bool:
+    return (x + y) == z
+
+
+data = []
+number = int(input('Enter number elements:'))
+for i in range(number):
+    data.append(int(input()))
+assert len(data) >= 3
+check_fibonacci(data)
+a, b, c = data[0], data[1], data[2]
+
+while data:
+    if not _check_window(a, b, c):
+        raise ValueError("Invalid data")
+    try:
+        a, b, c = b, c, data[3]
+    except IndexError:
+        break
+    data = data[1:]
+print("it's a fib sequence!")
