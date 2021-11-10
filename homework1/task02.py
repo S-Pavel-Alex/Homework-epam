8306006b1d8ffbf5ae3d6696da18c7f099fb821d
Pavel Smirnov <devil.9207@mail.ru>
2021-11-10 22:03:20 +0300

    Поместил логику внутрь функции

--- homework1/task02.py
+++ homework1/task02.py
@@ -10,28 +10,29 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 from typing import Sequence
 
 
-def check_fibonacci(data: Sequence[int]) -> bool:
-    return True
-
-
 def _check_window(x: int, y: int, z: int) -> bool:
     return (x + y) == z
 
 
-data = []
+seq = []
 number = int(input('Enter number elements:'))
 for i in range(number):
-    data.append(int(input()))
-assert len(data) >= 3
-check_fibonacci(data)
-a, b, c = data[0], data[1], data[2]
+    seq.append(int(input()))
 
-while data:
-    if not _check_window(a, b, c):
-        raise ValueError("Invalid data")
-    try:
-        a, b, c = b, c, data[3]
-    except IndexError:
-        break
-    data = data[1:]
-print("it's a fib sequence!")
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    assert len(data) >= 3
+    a, b, c = data[0], data[1], data[2]
+
+    while data:
+        if not _check_window(a, b, c):
+            raise ValueError("Invalid data")
+        try:
+            a, b, c = b, c, data[3]
+        except IndexError:
+            break
+        data = data[1:]
+    return True
+
+
+print(check_fibonacci(seq))
