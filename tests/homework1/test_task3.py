179c1c464b7b5ba94b0bbfe6b9207a327613df2e
Pavel Smirnov <devil.9207@mail.ru>
2021-11-13 01:23:43 +0300

    Добавил тест если строка пуста. Пофиксил в коде

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -1,16 +1,23 @@
-from homework1.task03 import find_maximum_and_minimum, file1, file2, file3
+from homework1.task03 import find_maximum_and_minimum
 
 
 def test_answer_positive():
     """File with two element and they 0"""
-    assert find_maximum_and_minimum(file1) == (0, 0)
+    assert find_maximum_and_minimum('C:\\Users\\forxb\\Documents\\home'
+                                    'work\\Homework-epam\\home'
+                                    'work1\\file1.txt') == (0, 0)
 
 
 def test_file_with_elements_plus_or_minus():
     """Elements positive and negative"""
-    assert find_maximum_and_minimum(file2) == (-125125345, 2151346)
+    assert find_maximum_and_minimum('C:\\Users\\forxb\\Documents\\home'
+                                    'work\\Homework-epam\\ho'
+                                    'mework1\\file2.txt') \
+           == (-125125345, 2151346)
 
 
 def test_file_with_space():
     """Between elements space"""
-    assert find_maximum_and_minimum(file3) == (-2323, 345)
+    assert find_maximum_and_minimum('C:\\Users\\forxb\\Documents\\home'
+                                    'work\\Homework-epam\\home'
+                                    'work1\\file3.txt') == (-2323, 345)
