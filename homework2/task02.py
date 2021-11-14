e33328e07d5bfee0a9420d7e2325bcf86735fe45
Pavel Smirnov <devil.9207@mail.ru>
2021-11-15 01:33:36 +0300

    Tack2 Не очень понял зачем нужно условие с n//2.

--- homework2/task02.py
+++ homework2/task02.py
@@ -22,15 +22,21 @@ from typing import List, Tuple
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
     my_dict = {}
+    final_dict = {}
     for element in inp:
         if element in my_dict:
             my_dict[element] += 1
         else:
             my_dict[element] = 1
-        max_element = max(my_dict.values())
-        min_element = min(my_dict.values())
+    for k, v in my_dict.items():
+        final_dict[v] = k
+    key_max = max(final_dict)
+    key_min = min(final_dict)
+    count = inp.count(final_dict[key_max])
+    if count >= len(inp)//2:
+        return final_dict[key_max], final_dict[key_min]
 
-    print(min_element, max_element)
 
 
-major_and_minor_elem([0, 0, 0, 1, 2, 2])
+
+print(major_and_minor_elem([2,2,1,1,1,2,2]))
