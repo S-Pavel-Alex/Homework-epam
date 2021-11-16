2126659f50bca81d938a808a2353a1e1bb76ad80
Pavel Smirnov <devil.9207@mail.ru>
2021-11-17 00:06:31 +0300

    Test task2

--- tests/homework2/test_task02.py
+++ tests/homework2/test_task02.py
@@ -1,9 +1,11 @@
 from homework2.task02 import major_and_minor_elem
 
 
-def test_two_elemenrs():
+def test_correct():
+    """Two elements, most common element more n//2"""
     assert major_and_minor_elem([0, 0, 0, 1]) == (0, 1)
 
 
-def test_four_elements():
-    assert major_and_minor_elem([1, 0, 0, 2, 2, 2, 2, 2, 3])
+def test_if_max_less_():
+    """If most common element less than n//2"""
+    assert major_and_minor_elem([1, 3, 3, 2, 2, 2, 2, 2, 3, 3, 1, 1]) is None
