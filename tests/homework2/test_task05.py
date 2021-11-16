f4e0027c18565bda8036b29c0a80bc2a987f0315
Pavel Smirnov <devil.9207@mail.ru>
2021-11-17 00:23:52 +0300

    Test5

--- tests/homework2/test_task05.py
+++ tests/homework2/test_task05.py
@@ -4,17 +4,20 @@ import string
 
 
 def test_with_one():
+    """With one argument is stop"""
     assert custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd',
                                                          'e', 'f']
 
 
 def test_with_two():
+    """Two arguments are start and stop"""
     assert custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i',
                                                               'j', 'k', 'l',
                                                               'm', 'n', 'o']
 
 
 def test_with_three():
+    """Three argumets are start, stop and step"""
     assert custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n',
                                                                   'l', 'j',
                                                                   'h']
