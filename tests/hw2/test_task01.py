ad02fecbb1de893d2948a8661d3c4f8b268fd538
Pavel Smirnov <devil.9207@mail.ru>
2021-11-18 00:42:02 +0300

    test1

--- tests/hw2/test_task01.py
+++ tests/hw2/test_task01.py
@@ -2,32 +2,28 @@ from hw2.task01 import (count_punctuation_chars, count_non_ascii_chars,
                         get_longest_diverse_words, get_rarest_char,
                         get_most_common_non_ascii_char)
 
-answer = [
-    'unmi\\u00dfverst\\u00e4ndliche', 'Wiederbelebungs\\u00fcbungen',
-    'r\\u00e9sistance-Bewegungen,', 'unver\\u00e4u\\u00dferlichen,',
-    '\\u00bbWaldg\\u00e4nger\\u00ab', 'Meinungs\\u00e4u\\u00dferung',
-    'Werkst\\u00e4ttenlandschaft', 'Souver\\u00e4nit\\u00e4tsan-',
-    'Br\\u00fcckenschl\\u00e4gen;', '\\u00bbFreiheitswahl\\u00ab'
-]
-
-text = 'data.txt'
-
 
 def test_long_words():
-    assert get_longest_diverse_words(text) == answer
+    assert get_longest_diverse_words('data.txt') == [
+        'unmi\\u00dfverst\\u00e4ndliche', 'Wiederbelebungs\\u00fcbungen',
+        'r\\u00e9sistance-Bewegungen,', 'unver\\u00e4u\\u00dferlichen,',
+        '\\u00bbWaldg\\u00e4nger\\u00ab', 'Meinungs\\u00e4u\\u00dferung',
+        'Werkst\\u00e4ttenlandschaft', 'Souver\\u00e4nit\\u00e4tsan-',
+        'Br\\u00fcckenschl\\u00e4gen;', '\\u00bbFreiheitswahl\\u00ab'
+    ]
 
 
 def test_rerest_char():
-    assert get_rarest_char(text) == ")"
+    assert get_rarest_char('data.txt') == ")"
 
 
 def test_count_punctuation_char():
-    assert count_punctuation_chars(text) == 4132
+    assert count_punctuation_chars('data.txt') == 4132
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars(text) == 2971
+    assert count_non_ascii_chars('data.txt') == 2971
 
 
 def test_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char(text) == 'daß'
+    assert get_most_common_non_ascii_char('data.txt') == 'daß'
--- tests/hw2/test_task01_1.py
+++
@@ -1,7 +0,0 @@
-from hw2.task01 import count_punctuation_chars
-
-text = 'data.txt'
-
-
-def test_count_punctuation_char():
-    assert count_punctuation_chars(text) == 4132
