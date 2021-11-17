020b157279c3d3a9fbbc7a317e7b16c654b590a0
Pavel Smirnov <devil.9207@mail.ru>
2021-11-18 01:07:20 +0300

    test_1_1

--- tests/hw2/test_task1_1.py
+++ tests/hw2/test_task1_1.py
@@ -1,5 +1,10 @@
+import os
+
 from hw2.task01 import count_punctuation_chars
+
+data_folder = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def test_count_punctuation_char():
-    assert count_punctuation_chars('data.txt') == 4132
+    file = os.path.join(data_folder, 'data.txt')
+    assert count_punctuation_chars(file) == 4132
