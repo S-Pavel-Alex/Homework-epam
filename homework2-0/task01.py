ea7751248bb8f1f12263368f3bcbfa761b49ce27
Pavel Smirnov <devil.9207@mail.ru>
2021-11-17 23:10:01 +0300

    New

---
+++ homework2-0/task01.py
@@ -0,0 +1,82 @@
+"""
+Given a file containing text. Complete using only default collections:
+    1) Find 10 longest words consisting from largest amount of unique symbols
+    2) Find rarest symbol for document
+    3) Count every punctuation char
+    4) Count every non ascii char
+    5) Find most common non ascii char for document
+"""
+from typing import List
+
+
+def get_longest_diverse_words(file_path: str) -> List[str]:
+    my_dict = {}
+    with open(file_path) as fi:
+        line_all = fi.read()
+        line_all = line_all.split()
+        for element in line_all:
+            if element not in my_dict:
+                my_dict[element] = 0
+        new_line = sorted(my_dict, key=len, reverse=True)
+        final_li = new_line[:10]
+        return final_li
+
+
+def get_rarest_char(file_path: str) -> str:
+    my_dir = {}
+    dir_all = {}
+    with open(file_path) as fi:
+        for line in fi:
+            for element in line:
+                if element in my_dir:
+                    my_dir[element] += 1
+                else:
+                    my_dir[element] = 1
+        for k, v in my_dir.items():
+            dir_all[v] = k
+        return dir_all[min(dir_all)]
+
+
+def count_punctuation_chars(file_path: str) -> int:
+    my_list = []
+    punctuation_list = ['.', ',', '?', '!']
+    with open(file_path) as fi:
+        for line in fi:
+            for element in line:
+                if element in punctuation_list:
+                    my_list.append(element)
+    return len(my_list)
+
+
+def count_non_ascii_chars(file_path: str) -> int:
+    my_list = []
+    with open(file_path, encoding='raw_unicode_escape') as fi:
+        for line in fi:
+            for element in line:
+                if not element.isascii():
+                    my_list.append(element)
+    return len(my_list)
+
+
+def get_most_common_non_ascii_char(file_path: str) -> str:
+    my_list = []
+    my_dir = {}
+    dir_all = {}
+    with open(file_path, encoding='raw_unicode_escape') as fi:
+        line_all = fi.read()
+        line = line_all.split()
+        for element in line:
+            if not element.isascii():
+                my_list.append(element)
+        for el in my_list:
+            if el in my_dir:
+                my_dir[el] += 1
+            else:
+                my_dir[el] = 1
+        for k, v in my_dir.items():
+            dir_all[v] = k
+        key_max = max(dir_all.keys())
+    return dir_all[key_max]
+
+
+print(get_longest_diverse_words('data.txt'))
---
+++ tests/homework2-0/test_task01.py
@@ -0,0 +1,34 @@
+from homework2.task01 import get_longest_diverse_words, get_rarest_char
+from homework2.task01 import count_punctuation_chars, count_non_ascii_chars
+from homework2.task01 import get_most_common_non_ascii_char
+
+answer = [
+    'unmi\\u00dfverst\\u00e4ndliche', 'Wiederbelebungs\\u00fcbungen',
+    'r\\u00e9sistance-Bewegungen,', 'unver\\u00e4u\\u00dferlichen,',
+    '\\u00bbWaldg\\u00e4nger\\u00ab', 'Meinungs\\u00e4u\\u00dferung',
+    'Werkst\\u00e4ttenlandschaft', 'Souver\\u00e4nit\\u00e4tsan-',
+    'Br\\u00fcckenschl\\u00e4gen;', '\\u00bbFreiheitswahl\\u00ab'
+]
+
+
+text = 'data.txt'
+
+
+def test_long_words():
+    assert get_longest_diverse_words(text) == answer
+
+
+def test_rerest_char():
+    assert get_rarest_char(text) == ")"
+
+
+def test_count_punctuation_char():
+    assert count_punctuation_chars(text) == 4132
+
+
+def test_count_non_ascii_chars():
+    assert count_non_ascii_chars(text) == 2971
+
+
+def test_most_common_non_ascii_char():
+    assert get_most_common_non_ascii_char(text) == 'daß'
