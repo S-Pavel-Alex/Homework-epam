18517254a22049d16747ba44ba2418593cfcc1bf
Pavel Smirnov <devil.9207@mail.ru>
2021-11-25 19:27:09 +0300

    f

---
+++ tests/hw2/test_task1_2.py
@@ -0,0 +1,16 @@
+import os
+
+from hw2.task01 import get_longest_diverse_words
+
+data_folder = os.path.join(os.path.dirname(__file__), 'data')
+file = os.path.join(data_folder, 'data1.txt')
+
+
+def test_long_words():
+    assert get_longest_diverse_words(file) == [
+        'unmi\\u00dfverst\\u00e4ndliche', 'Wiederbelebungs\\u00fcbungen',
+        'r\\u00e9sistance-Bewegungen,', 'unver\\u00e4u\\u00dferlichen,',
+        '\\u00bbWaldg\\u00e4nger\\u00ab', 'Meinungs\\u00e4u\\u00dferung',
+        'Werkst\\u00e4ttenlandschaft', 'Souver\\u00e4nit\\u00e4tsan-',
+        'Br\\u00fcckenschl\\u00e4gen;', '\\u00bbFreiheitswahl\\u00ab'
+    ]
---
+++ tests/hw2/test_task4.py
@@ -0,0 +1,9 @@
+from hw2.task04 import funct
+
+
+def test_correct_work():
+    some = 100, 2
+    val_1 = funct(*some)
+    val_2 = funct(*some)
+
+    assert val_1 is val_2
