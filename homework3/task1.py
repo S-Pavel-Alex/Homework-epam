4dc371da07d274b6d3e73eadabdb2110c29089c1
Pavel Smirnov <devil.9207@mail.ru>
2021-11-26 02:04:21 +0300

    task1 and test task1

---
+++ homework3/task1.py
@@ -0,0 +1,25 @@
+import functools
+
+
+def cache(time):
+    for _ in range(time):
+        def oder(func):
+            my_dict = dict()
+
+            @functools.wraps(func)
+            def wrapper(*args):
+                if args not in my_dict:
+                    my_dict[args] = func(*args)
+                return my_dict[args]
+            return wrapper
+        return oder
+
+
+total = 0
+
+
+@cache(time=3)
+def my_fun(item):
+    global total
+    total += 1
+    return item, total
