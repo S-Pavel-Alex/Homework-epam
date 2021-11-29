b5c266193233bd0d4a3d2e882aee41e51c62fb76
Pavel Smirnov <devil.9207@mail.ru>
2021-11-30 00:33:03 +0300

    task1

--- homework5/task1.py
+++ homework5/task1.py
@@ -44,15 +44,15 @@ class Homework:
     def __init__(self, text, deadline):
         self.text = text            # text lesson
         self.deadline = datetime.timedelta(deadline)    # how many times
-        self.created = datetime.datetime.now()
+        self.created = datetime.datetime.now()          # time create lesson
+        self.final_time = self.created + self.deadline
 
-
-    def is_active(self,):
-
-
-
-
-
+    def is_active(self):
+        time_for_test = datetime.datetime.now()
+        if time_for_test > self.final_time:
+            return 'You are lose'
+        else:
+            return True
 
 
 class Teacher:
@@ -60,8 +60,9 @@ class Teacher:
         self.first_name = first_name
         self.last_name = last_name
 
-    def create_homework(self, text: str, how_many_days: int):
-        pass
+    @staticmethod
+    def create_homework(text: str, how_many_days: int):
+        return Homework(text, how_many_days)
 
 
 class Student:
@@ -69,24 +70,27 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
 
+    @staticmethod
+    def do_homework(homework):
+        return homework
+
 
 if __name__ == '__main__':
     teacher = Teacher('Daniil', 'Shadrin')
     student = Student('Roman', 'Petrov')
 
-
-    # expired_homework = teacher.create_homework('Learn functions', 0)
-    # expired_homework.created  # Example: 2019-05-26 16:44:30.688762
-    # expired_homework.deadline  # 0:00:00
-    # expired_homework.text  # 'Learn functions'
+    expired_homework = teacher.create_homework('Learn functions', 0)
+    print(expired_homework.created)  # Example: 2019-05-26 16:44:30.688762
+    print(expired_homework.deadline)  # 0:00:00
+    print(expired_homework.text)  # 'Learn functions'
     #
-    # # create function from method and use it
-    # create_homework_too = teacher.create_homework
-    # oop_homework = create_homework_too('create 2 simple classes', 5)
-    # oop_homework.deadline  # 5 days, 0:00:00
+    # create function from method and use it
+    create_homework_too = teacher.create_homework
+    oop_homework = create_homework_too('create 2 simple classes', 5)
+    print(oop_homework.deadline)  # 5 days, 0:00:00
     #
-    # student.do_homework(oop_homework)
-    # student.do_homework(expired_homework)  # You are late
+    print(student.do_homework(oop_homework))
+    print(student.do_homework(expired_homework))  # You are late
 
 # t = datetime.datetime.now()
 # d = datetime.timedelta()
