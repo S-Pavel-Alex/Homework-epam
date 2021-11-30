3a68d3c12c9eba7a1bfb5477cd8a8e11c5aa9652
Pavel Smirnov <devil.9207@mail.ru>
2021-11-30 22:08:42 +0300

    task3 and test_task3

--- homework2/task3.py
+++ homework2/task3.py
@@ -15,27 +15,9 @@ assert combinations([1, 2], [3, 4]) == [
 ]
 """
 import itertools
-from itertools import product
+
 from typing import Any, List
 
 
 def combinations(*args: List[Any]) -> List[List]:
     return list(map(lambda x: list(x), [x for x in itertools.product(*args)]))
-
-
-
-print(combinations([2, 3]))
-# # print(list(product([1, 2], [3, 4])))
-#
-# def product2345(*args):
-#     # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-#     # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-#     pools = [tuple(pool) for pool in args]
-#     result = [[]]
-#     for pool in pools:
-#         result = [x+[y] for x in result for y in pool]
-#     for prod in result:
-#         yield tuple(prod)
-#     return result
-#
-# # print(list(product2345([1, 2], [3, 4])))
