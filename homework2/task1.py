666a0a49802c4fa2309b62f048b2e38b3e921289
Pavel Smirnov <devil.9207@mail.ru>
2021-12-01 06:50:54 +0300

    fixed CI

--- homework2/task1.py
+++ homework2/task1.py
@@ -6,10 +6,9 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
+from typing import List
 
 import string
-
-from typing import List
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
--- homework2/task3.py
+++ homework2/task3.py
@@ -14,10 +14,9 @@ assert combinations([1, 2], [3, 4]) == [
     [2, 4],
 ]
 """
+from typing import Any, List
 
 import itertools
-
-from typing import Any, List
 
 
 def combinations(*args: List[Any]) -> List[List]:
