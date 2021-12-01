76947b57c0f83635f37f8aac2f6a8b21952bed0d
Pavel Smirnov <devil.9207@mail.ru>
2021-12-01 06:58:45 +0300

    fixed CI

--- homework2/task3.py
+++ homework2/task3.py
@@ -14,9 +14,8 @@ assert combinations([1, 2], [3, 4]) == [
     [2, 4],
 ]
 """
-from typing import Any, List
-
 import itertools
+from typing import Any, List
 
 
 def combinations(*args: List[Any]) -> List[List]:
