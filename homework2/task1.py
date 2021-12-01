e91323911a4bf4621345e9b4baf778d6c17ab1f2
Pavel Smirnov <devil.9207@mail.ru>
2021-12-01 06:52:05 +0300

    fixed CI

--- homework2/task1.py
+++ homework2/task1.py
@@ -6,6 +6,7 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
+
 from typing import List
 
 import string
