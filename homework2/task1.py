f1f6fb7c9af6aa80d4e23f7724dffa7e95af2eda
Pavel Smirnov <devil.9207@mail.ru>
2021-12-02 19:25:15 +0300

    no

--- homework2/task1.py
+++ homework2/task1.py
@@ -13,11 +13,11 @@ from typing import List
 def get_longest_diverse_words(file_path: str) -> List[str]:
     my_dict = {}
     with open(file_path, encoding='raw_unicode_escape') as fi:
-        line_all = fi.read()
-        line_all = line_all.split()
-        for element in line_all:
-            if element not in my_dict:
-                my_dict[element] = 0
+        for line in fi:
+            line = line.split()
+            for element in line:
+                if element not in my_dict:
+                    my_dict[element] = 0
         new_line = sorted(my_dict, key=len, reverse=True)
         final_li = new_line[:10]
         return final_li
