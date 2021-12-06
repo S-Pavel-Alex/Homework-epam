f1c2e49aba6bcfffff6ac1ee9c0fc78e79e31cd8
Pavel Smirnov <devil.9207@mail.ru>
2021-12-06 16:10:57 +0300

    task1 and test_task1

--- homework3/task1.py
+++ homework3/task1.py
@@ -10,15 +10,10 @@ def cache(time):
             bound = sig.bind(*args, **kwargs)
             bound.apply_defaults()
             key = str(bound)
-            if key not in my_dict:
-                my_dict[key] = [func(*args, **kwargs), time]
-                my_dict[key][1] -= 1
-                return my_dict[key][0]
-            elif key in my_dict and my_dict[key][1] != 0:
+            if key in my_dict and my_dict[key][1] != 0:
                 my_dict[key][1] -= 1
                 return my_dict[key][0]
             else:
-                del my_dict[key]
                 my_dict[key] = [func(*args, **kwargs), time]
                 my_dict[key][1] -= 1
                 return my_dict[key][0]
--- tests/homework3/test_task1.py
+++ tests/homework3/test_task1.py
@@ -1,7 +1,7 @@
 from homework3.task1 import cache
 
 
-def test_cache():
+def test_cache_with_3():
     invoked = 0
 
     @cache(time=3)
@@ -14,3 +14,18 @@ def test_cache():
     for _ in range(9):
         assert my_function(3) == 5
     assert invoked == 3
+
+
+def test_cache_with_2():
+    invoked = 0
+
+    @cache(time=2)
+    def my_function(a, b=2):
+        nonlocal invoked
+        invoked += 1
+        return a + b
+
+    assert invoked == 0
+    for _ in range(10):
+        assert my_function(3) == 5
+    assert invoked == 5
