8ab160480e45c7becb8e0787f8e9492a850fbb39
Pavel Smirnov <devil.9207@mail.ru>
2021-12-08 00:01:05 +0300

    task2 so so

---
+++ homework5/task2.py
@@ -0,0 +1,41 @@
+import functools
+
+
+def print_result(func):
+    # @saver_decorator(func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result
+
+    return wrapper
+
+
+def saver_decorator(func):
+    def wrapper(*args):
+        f1 = func.__name__
+        f2 = func.__doc__
+        print(f1, f2)
+    return wrapper
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___"""
+    return functools.reduce(lambda x, y: x + y, args)
+
+
+saver_decorator(custom_sum)
+
+
+if __name__ == "__main__":
+    custom_sum([1, 2, 3], [4, 5])
+    custom_sum(1, 2, 3, 4)
+
+    print(custom_sum.__doc__)
+    print(custom_sum.__name__)
+    without_print = custom_sum.__original_func
+
+    # the result returns without printing
+    without_print(1, 2, 3, 4)
