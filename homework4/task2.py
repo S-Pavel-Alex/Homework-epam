05c9480b58cf043abcc6b226c27b75aadba89c03
Pavel Smirnov <devil.9207@mail.ru>
2021-12-08 00:36:24 +0300

    task2 test_task2

--- homework4/task2.py
+++ homework4/task2.py
@@ -1,14 +1,19 @@
-import urllib.request
+from unittest.mock import Mock
 
 
 def count_dots_on_i(url: str) -> int:
     """Count i item"""
-    text = connect_url(url)
+    if connect_url_mock(url).status_code != 200:
+        raise ValueError(f"Unreachable {url}")
+    text = connect_url_mock(url).text
     total = text.count('i')
     return total
 
 
-def connect_url(url: str):
+def connect_url_mock(url: str):
     """Create connect and read text"""
-    response = urllib.request.urlopen(url)
-    return response.read().decode('utf-8')
+    mock_object = Mock()
+    mock_object.url = url
+    mock_object.text = 'i say i'
+    mock_object.status_code = 200
+    return mock_object
---
+++ tests/homework4/test_task2.py
@@ -0,0 +1,5 @@
+from homework4.task2 import *
+
+
+def test_count_dots_on_i_positive():
+    assert count_dots_on_i('example.com') == 2
