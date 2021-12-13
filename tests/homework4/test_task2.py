e7117efae778534573255c9659996c65159d29b4
Pavel Smirnov <devil.9207@mail.ru>
2021-12-13 22:11:46 +0300

    import requests problem

--- tests/homework4/test_task2.py
+++ tests/homework4/test_task2.py
@@ -2,31 +2,31 @@ import unittest.mock
 
 import pytest
 
-import homework4.task2
+from homework4.task2 import count_dots_on_i
 
 
 def test_count_dots_on_i_positive():
-    assert homework4.task2.count_dots_on_i('https://example.com/') == 59
+    assert count_dots_on_i('https://example.com/') == 59
 
 
 def test_mock_count_dots_on_i_positive():
     with unittest.mock.patch('requests.get') as mock_ob:
         mock_ob().text = 'i'
-        assert homework4.task2.count_dots_on_i('https://example.com/') == 1
+        assert count_dots_on_i('https://example.com/') == 1
 
 
 def test_mock_count_dots_without_i_positive():
     with unittest.mock.patch('requests.get') as mock_ob:
         mock_ob().text = 'not'
-        assert homework4.task2.count_dots_on_i('https://example.com/') == 0
+        assert count_dots_on_i('https://example.com/') == 0
 
 
 def test_mock_count_dots_without_text_positive():
     with unittest.mock.patch('requests.get') as mock_ob:
         mock_ob().text = ''
-        assert homework4.task2.count_dots_on_i('https://example.com/') == 0
+        assert count_dots_on_i('https://example.com/') == 0
 
 
 def test_count_dots_incorrect_url():
     with pytest.raises(ValueError):
-        homework4.task2.count_dots_on_i('https://sefgg.com/')
+        count_dots_on_i('https://sefgg.com/')
