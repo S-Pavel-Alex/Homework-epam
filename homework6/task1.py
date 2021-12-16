4af0aa75056bdd0505cec5ed7584348154b8d06e
Pavel Smirnov <devil.9207@mail.ru>
2021-12-16 20:52:12 +0300

    task1 improvisation

--- homework6/task1.py
+++ homework6/task1.py
@@ -3,7 +3,7 @@ def instances_counter(cls):
         count_instance = 0
 
         def __init__(self, *args, **kwarg):
-            self.obj = cls(*args, **kwarg)
+            self._obj = cls(*args, **kwarg)
             super().__init__(*args, **kwarg)
             ModifiedClass.count_instance += 1
 
@@ -17,12 +17,7 @@ def instances_counter(cls):
             cls.count_instance = 0
             return value_instance
 
-        def __getattribute__(self, item):
-            try:
-                x = super().__getattribute__(item)
-            except AttributeError:
-                return self.obj.__getattribute__(item)
-            else:
-                return x
+        def __getattr__(self, atr):
+            return getattr(self._obj, atr)
 
     return ModifiedClass
