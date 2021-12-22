1e15fac75c5aa2af67885796920c2d9345879fd1
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2021-12-22 12:34:32 +0300

    add test for object

--- tests/homework6/test_task1.py
+++ tests/homework6/test_task1.py
@@ -8,6 +8,9 @@ class User:
 
 def test_positive_count_instance_get_create():
     user1, user2, user3 = User(), User(), User()
+    assert user1.get_created_instances() == 3
+    assert user2.get_created_instances() == 3
+    assert user3.get_created_instances() == 3
     assert User.get_created_instances() == 3
 
 
@@ -15,6 +18,9 @@ def test_positive_count_instance_reset_instance():
     user1, user2, user3 = User(), User(), User()
     assert User.reset_instances_counter() == 3
     assert User.get_created_instances() == 0
+    assert user1.get_created_instances() == 0
+    assert user2.get_created_instances() == 0
+    assert user3.get_created_instances() == 0
 
 
 def test_positive_with_inheritance():
@@ -23,5 +29,7 @@ def test_positive_with_inheritance():
 
     user = User()
     assert user.get_created_instances() == 1
+    assert User.get_created_instances() == 1
     user2 = User2()
     assert user2.get_created_instances() == 1
+    assert User2.get_created_instances() == 1
