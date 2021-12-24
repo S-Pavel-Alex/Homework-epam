0afad5af1ee109a59c300e226d88f002e9aacf9f
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2021-12-24 21:54:21 +0300

    task1 and test task1

---
+++ tests/homework7/test_task1.py
@@ -0,0 +1,27 @@
+from homework7.task1 import find_occurrences
+
+example_tree = {
+    "first": ["RED", "BLUE"],
+    "second": {
+        "simple_key": [True, "list", 4, "RED", "valued"],
+    },
+    "third": {
+        "abc": "BLUE",
+        "jhl": "RED",
+        "complex_key": {
+            "key1": "value1",
+            "key2": "RED",
+            "key3": ["a", "lot", "of", "values", {"nested_key": 4}],
+        }
+     },
+    "fourth": "RED",
+}
+
+
+def test_find_occurrences():
+    assert find_occurrences(example_tree, "RED") == 5
+    assert find_occurrences(example_tree, 4) == 2
+    assert find_occurrences(example_tree, 'example') == 0
+    assert find_occurrences(example_tree, '') == 0
+    assert find_occurrences(example_tree, True) == 1
+    assert find_occurrences(example_tree, 'fourth') == 1
