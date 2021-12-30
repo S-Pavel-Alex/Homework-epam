e580f9eba7371b803d2b54b0b114f855b4d388dd
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2021-12-30 19:53:37 +0300

    start

--- homework8/task1.py
+++ homework8/task1.py
@@ -1,10 +1,22 @@
 class KeyValueStorage:
+    dict_class = {}
+
     def __init__(self, file):
         self.file = file
-        # with open(self.file) as document:
-        #     document.read()
+        try:
+            with open(file) as document:
+                for line in document:
+                    key, value = line.strip().split('=')
+                    if not key.isidentifier():
+                        raise ValueError(key)
+                    if KeyValueStorage.__get
+                        KeyValueStorage.__setattr__(self, key, value)
+        except FileNotFoundError:
+            print(f'File {self.file} not founded')
+
+    def __getitem__(self, key):
+        return getattr(self, key)
 
 
-
-k = KeyValueStorage('text.txt')
+k = KeyValueStorage('task1.txt')
 print(k.__dict__)
