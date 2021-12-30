368260d4f71ffd3ac99057cdf72c0559fc28cca3
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2021-12-31 01:12:30 +0300

    task3

---
+++ homework9/task3.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from typing import Optional, Callable
+
+
+def count_lines(file_path: Path):
+    with open(file_path) as file:
+        count = 0
+        for _ in file:
+            count += 1
+    return count
+
+
+def count_with_tokenizer(file_path: Path, tokenizer: Optional[Callable]):
+    with open(file_path) as file:
+        count = 0
+        for line in file:
+            token = tokenizer(line)
+            count += len(token)
+    return count
+
+
+def universal_file_counter(
+    dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
+) -> int:
+    files = [i for i in dir_path.glob(f'**/*.{file_extension}') if i.is_file()]
+    if tokenizer:
+        all_token = sum(count_with_tokenizer(file, tokenizer)
+                        for file in files)
+        return all_token
+    return sum(count_lines(file) for file in files)
