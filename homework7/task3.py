09f951de49f98dfa99d7b2c14d1987d971aa0d34
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2021-12-30 14:40:18 +0300

    fixed task3

--- homework7/task3.py
+++ homework7/task3.py
@@ -1,35 +1,23 @@
 from typing import List
+from itertools import chain
 
 
-def combination_x_winner(board: List[List]):
-    if board[0][0] == board[0][1] == board[0][2] == 'x' or \
-     board[1][0] == board[1][1] == board[1][2] == 'x' or \
-     board[2][0] == board[2][1] == board[2][2] == 'x' or \
-     board[0][0] == board[1][0] == board[2][0] == 'x' or \
-     board[0][1] == board[1][1] == board[2][1] == 'x' or \
-     board[0][2] == board[1][2] == board[2][2] == 'x' or \
-     board[0][0] == board[1][1] == board[2][2] == 'x' or \
-     board[0][2] == board[1][1] == board[2][0] == 'x':
-        return True
-
-
-def combination_o_winner(board: List[List]):
-    if board[0][0] == board[0][1] == board[0][2] == 'o' or \
-     board[1][0] == board[1][1] == board[1][2] == 'o' or \
-     board[2][0] == board[2][1] == board[2][2] == 'o' or \
-     board[0][0] == board[1][0] == board[2][0] == 'o' or \
-     board[0][1] == board[1][1] == board[2][1] == 'o' or \
-     board[0][2] == board[1][2] == board[2][2] == 'o' or \
-     board[0][0] == board[1][1] == board[2][2] == 'o' or \
-     board[0][2] == board[1][1] == board[2][0] == 'o':
-        return True
+def winner_combination(board: List[List]):
+    combination = [[board[0][0], board[0][1], board[0][2]],
+                   [board[1][0], board[1][1], board[1][2]],
+                   [board[2][0], board[2][1], board[2][2]],
+                   [board[0][0], board[1][0], board[2][0]],
+                   [board[0][1], board[1][1], board[2][1]],
+                   [board[0][2], board[1][2], board[2][2]],
+                   [board[0][0], board[1][1], board[2][2]],
+                   [board[0][2], board[1][1], board[2][0]]]
+    for row in combination:
+        if row[0] == row[1] == row[2] != '-':
+            return row[0]
 
 
 def unfinished(board: List[List]):
-    for row in range(3):
-        for col in range(3):
-            if board[row][col] == '-':
-                return True
+    return '-' in list(chain(*board))
 
 
 def tic_tac_toe_checker(board: List[List]) -> str:
@@ -38,11 +26,11 @@ def tic_tac_toe_checker(board: List[List]) -> str:
     :param board: this combination on board
     :type board: List[List]
     """
-    if combination_x_winner(board):
+    if winner_combination(board) == 'x':
         return 'x wins!'
-    if combination_o_winner(board):
+    if winner_combination(board) == 'o':
         return 'o wins!'
-    if unfinished(board) is True:
+    if unfinished(board):
         return 'unfinished!'
     else:
         return 'draw!'
