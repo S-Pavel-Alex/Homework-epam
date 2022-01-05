37aec43aa7374c747adba69f89514ed0a4de159a
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2022-01-05 20:15:07 +0300

    fixed

--- tests/homework9/test_task1.py
+++ tests/homework9/test_task1.py
@@ -1,17 +1,26 @@
 import pytest
+import os
 
 from homework9.task1 import merge_sorted_files
 
 
+file1 = os.path.join(os.path.dirname(__file__), 'file1.txt')
+file2 = os.path.join(os.path.dirname(__file__), 'file2.txt')
+file3 = os.path.join(os.path.dirname(__file__), 'file3.txt')
+file4 = os.path.join(os.path.dirname(__file__), 'file4.txt')
+file5 = os.path.join(os.path.dirname(__file__), 'file5.txt')
+file6 = os.path.join(os.path.dirname(__file__), 'file6.txt')
+
+
 @pytest.mark.parametrize("test_input, expected",
                          [
-                             (['file1.txt', 'file2.txt'], [1, 2, 3, 4, 6]),
-                             (['file3.txt', 'file2.txt'], [2, 4, 6]),
-                             (['file4.txt', 'file2.txt'], [-5, -1, 2, 4, 6]),
-                             (['file5.txt', 'file2.txt'], [2, 4, 6, 10,
-                                                           50, 60, 1000]),
-                             (['file3.txt', 'file6.txt'], []),
-                             (['file1.txt', 'file2.txt', 'file4.txt'],
+                             ([file1, file2], [1, 2, 3, 4, 6]),
+                             ([file3, file2], [2, 4, 6]),
+                             ([file4, file2], [-5, -1, 2, 4, 6]),
+                             ([file5, file2], [2, 4, 6, 10,
+                                               50, 60, 1000]),
+                             ([file3, file6], []),
+                             ([file1, file2, file4],
                               [-5, -1, 1, 2, 3, 4, 6]),
                              ([], [])
                          ])
