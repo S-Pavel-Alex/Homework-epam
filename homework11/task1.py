f9540ec16601b80a71b585261f5457b0a42cdea5
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2022-01-13 01:51:15 +0300

    task1

--- homework11/task1.py
+++ homework11/task1.py
@@ -2,7 +2,7 @@ class SimplifiedEnum(type):
     def __new__(mcs, name: str, superclass: tuple, dct: dict):
         cls_key = f'_{name}__keys'
         cls_instance = super().__new__(mcs, name, superclass, dct)
-        if getattr(cls_instance, cls_key, None):
+        if hasattr(cls_instance, cls_key):
             for key in getattr(cls_instance, cls_key):
                 setattr(cls_instance, key, key)
         return cls_instance
