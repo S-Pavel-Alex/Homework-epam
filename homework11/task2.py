414991f265914018d2de20d84b003add786537e0
Pavel Smirnov <93800032+S-Pavel-Alex@users.noreply.github.com>
2022-01-14 14:08:56 +0300

    task2

--- homework11/task2.py
+++ homework11/task2.py
@@ -1,25 +1,3 @@
-from abc import ABC, abstractmethod
-
-
-class Discount(ABC):
-    @staticmethod
-    @abstractmethod
-    def count_discount():
-        pass
-
-
-class MorningDiscount(Discount):
-    @staticmethod
-    def count_discount():
-        return 0.25
-
-
-class ElderDiscount(Discount):
-    @staticmethod
-    def count_discount():
-        return 0.8
-
-
 class Oder:
     def __init__(self, price, discount=0):
         self.price = price
@@ -29,4 +7,4 @@ class Oder:
         """Calculate final price"""
         if self.discount == 0:
             return self.price
-        return self.price - self.price * self.discount.count_discount()
+        return self.price - self.discount(self.price)
--- tests/homework11/test_task2.py
+++ tests/homework11/test_task2.py
@@ -1,4 +1,16 @@
-from homework11.task2 import ElderDiscount, MorningDiscount, Oder
+from homework11.task2 import Oder
+
+
+def morning_discount(order):
+    if order < 5000:
+        return 0.4 * order
+    return 0.5 * order
+
+
+def elder_discount(order):
+    if order < 5000:
+        return 0.1 * order
+    return 0.2 * order
 
 
 def test_without_discount():
@@ -7,13 +19,25 @@ def test_without_discount():
     assert oder.final_price() == 100
 
 
-def test_morning_discount():
-    """With morning discount"""
-    oder = Oder(100, MorningDiscount())
-    assert oder.final_price() == 75
+def test_morning_discount_less():
+    """With morning discount less 5000"""
+    oder = Oder(100, morning_discount)
+    assert oder.final_price() == 60
 
 
-def test_elder_discount():
-    """With elder discount"""
-    oder = Oder(100, ElderDiscount())
-    assert oder.final_price() == 20
+def test_morning_discount_high():
+    """With morning discount high  5000"""
+    oder = Oder(10000, morning_discount)
+    assert oder.final_price() == 5000
+
+
+def test_elder_discount_less():
+    """With elder discount less 5000"""
+    oder = Oder(100, elder_discount)
+    assert oder.final_price() == 90
+
+
+def test_elder_discount_high():
+    """With morning discount high  5000"""
+    oder = Oder(10000, elder_discount)
+    assert oder.final_price() == 8000
